5c2a8499ff7525e8d1f6aaa450e3fb2b4aa921c8
Tratten Production <trattenprod@ksu-srv-django.ksu.lan>
2010-03-12 17:24:36 +0100

    Moved DB settings to local_settings

--- tratten/development.py
+++ tratten/development.py
@@ -6,9 +6,6 @@ from tratten.settings import *
 DEBUG=True
 TEMPLATE_DEBUG=DEBUG
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = 'tratten.db'
-
 try:
     from tratten.local_settings import *
 except ImportError:
--- tratten/production.py
+++ tratten/production.py
@@ -3,12 +3,6 @@
 
 from tratten.settings import *
 
-DATABASE_ENGINE = 'mysql'
-DATABASE_NAME = 'tratten'
-DATABASE_USER = 'tratten'
-# DATABASE_HOST = ''        # Set to empty string for localhost.
-# DATABASE_PORT = ''        # Set to empty string for default.
-
 try:
     from tratten.local_settings import *
 except ImportError:
